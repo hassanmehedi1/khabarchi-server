74ff0c6a6c50cc0644144d2a2431aafb0db9b0a5
Mehedi Hassan <rehan51322@gmail.com>
2022-05-08 23:27:30 +0600

    JWT bug fixed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -8,6 +9,22 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 //middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+  // console.log("inside verifyJWT", authHeader);
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ecv2a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -20,6 +37,14 @@ async function run() {
   try {
     await client.connect();
     const foodCollection = client.db("foodInventory").collection("food");
+    // AUTH
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
 
     //GET
     app.get("/food", async (req, res) => {
@@ -37,15 +62,20 @@ async function run() {
       res.send(food);
     });
 
-    app.get('/myitem', async (req, res)=>{
+    app.get("/myitem", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
       const email = req.query.email;
       console.log(email);
-      const query = {email}
-      const cursor = foodCollection.find(query)
-      const orders = await cursor.toArray();
-      res.send(orders);
-    })
-    
+      if (email === decodedEmail) {
+        const query = { email };
+        const cursor = foodCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      } else {
+        res.status(403).send({ message: "Forbidden Access" });
+      }
+      
+    });
 
     //update
     //delivered by one one
@@ -63,34 +93,26 @@ async function run() {
           quantity: newQuantity.quantity - 1,
         },
       };
-      const result = await foodCollection.updateOne(
-        filter,
-        updateDoc,
-        option
-      );
+      const result = await foodCollection.updateOne(filter, updateDoc, option);
       res.send(result);
     });
 
-     app.put("/update/:id", async (req, res) => {
-       const id = req.params.id;
+    app.put("/update/:id", async (req, res) => {
+      const id = req.params.id;
       //  console.log(id);
 
-       const newQuantity = req.body;
+      const newQuantity = req.body;
       //  console.log(newQuantity);
-       const filter = { _id: ObjectId(id) };
-       const option = { upsert: true };
-       const updateDoc = {
-         $set: {
-           quantity: newQuantity.newQuantity,
-         },
-       };
-       const result = await foodCollection.updateOne(
-         filter,
-         updateDoc,
-         option
-       );
-       res.send(result);
-     });
+      const filter = { _id: ObjectId(id) };
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: {
+          quantity: newQuantity.newQuantity,
+        },
+      };
+      const result = await foodCollection.updateOne(filter, updateDoc, option);
+      res.send(result);
+    });
 
     //POST
     app.post("/food", async (req, res) => {
